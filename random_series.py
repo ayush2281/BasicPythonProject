ad9b205c723fe7182436617a46cdf85d1ea442f9
AyushSinghRajput1299 <134182912+AyushSinghRajput1366@users.noreply.github.com>
2024-02-16 22:13:42 +0530

    Add files via upload

---
+++ random_series.py
@@ -0,0 +1,15 @@
+from random import choice
+
+# create list of series
+print("You can watch this series")
+series = ['perpherals..', 'matrix..' , 'john wick..' , 'lucifer..', 'mother of dragon..', 'the boys..', 'reacher..']
+series2 = ['Game Of Thrones..', 'friends..', 'journey2..', 'the vampire diaries..', 'the flash..',
+        'spider man..', ' the tommorow war..', ' mission impossible..']
+
+
+
+# print a random series from the series list
+# print(choice(series))
+print(choice(series2))
+print("And this best series i ever watch")
+print('please Share the ratting at the end How i guess......')
